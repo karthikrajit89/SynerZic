00f7248f6a82d9a952735491510435bff438e8ea
Karthik <Karthikeyan.kanagaraj@leapdev.io>
2022-05-07 20:34:45 +1000

    Updated for Client demo

--- src/components/impl/api.ts
+++ src/components/impl/api.ts
@@ -1,36 +1,39 @@
-import { timeStamp } from 'console';
-import {Implementation} from '../impl/impl';
-import { SiteContent } from '../site/site';
+import { timeStamp } from "console";
+import { Implementation } from "../impl/impl";
+import { SiteContent } from "../site/site";
 
+export class api {
+  private impl: Implementation = new Implementation("");
 
- export class api{
-    private impl :Implementation = new Implementation('');
+  createMenu(): SiteContent.Menu[] {
+    return this.impl.createMenu();
+  }
 
-    createMenu():SiteContent.Menu[]{
-        return this.impl.createMenu();
-    }
+  createIntro(): string {
+    return this.impl.createIntro();
+  }
 
-    createIntro():string{
-        return this.impl.createIntro();
-    }
+  createAbout(): SiteContent.About {
+    return this.impl.createAbout();
+  }
 
-    createAbout():SiteContent.About{
-        return this.impl.createAbout();
-    }
+  createService(): SiteContent.Service {
+    return this.impl.createService();
+  }
 
-    createService() : SiteContent.Service{
-        return this.impl.createService();
-    }
+  createConatct(): SiteContent.Contact {
+    return this.impl.createContact();
+  }
 
-    createConatct() : SiteContent.Contact{
-        return this.impl.createContact();
-    }
+  createMessage(request: SiteContent.Message): boolean {
+    return this.impl.createMessage(request);
+  }
 
-    createMessage(request: SiteContent.Message) : boolean {
-        return this.impl.createMessage(request)
-    }
+  createFooter(): SiteContent.Footer {
+    return this.impl.createFooter();
+  }
 
-    createFooter() : SiteContent.Footer{
-      return  this.impl.createFooter();
-    }
+  createClientShow(): SiteContent.ClientShow {
+    return this.impl.createClientShow();
+  }
 }
